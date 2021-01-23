e23ecadeb575f1b01028ad6e8fa69bdfc854a788
Oktawian <oktawian.perlowski@gmail.com>
2021-01-23 01:21:39 +0100

    Implemented logout, showing some stuff only to logged in users

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user/user.service";
 
 @Component({
   selector: 'app-side-nav',
@@ -7,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-    loggedIn = false;
-
-    constructor() { }
+    constructor(private userService: UserService) { }
 
     ngOnInit(): void {
+    }
+
+    logout(){
+        this.userService.logout();
+    }
+
+    isSignedIn(){
+        return this.userService.getIsSignedIn();
     }
 
 
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -10,17 +10,33 @@ const baseUrl = 'https://localhost:8015 ';
     providedIn: 'root'
 })
 export class UserService {
+    isSignedIn = false;
     constructor(private http: HttpClient) { }
 
 
     signin(user: User): Observable<any> {
-        return this.http.post('http://localhost:8015/user/signin', user, {observe: 'response'});
+        return this.http.post('http://localhost:8015/user/signin', user, {observe: 'response'})
+            .pipe(map(response => {
+                if(response.status === 200){
+                    this.isSignedIn = true;
+                }
+                return response;
+            }));
     }
+
     signup(user: User): Observable<any> {
         return this.http.post('$(baseUrl)/signup/', user,
             {observe: 'response' as 'body'})
             .pipe(map(vUser => {
                 return vUser;
             }));
+    }
+
+    logout(){
+        this.isSignedIn = false;
+    }
+
+    getIsSignedIn(){
+        return this.isSignedIn;
     }
 }
